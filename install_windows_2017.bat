b349e56caf6bd51d2e1b423e39fb51c9b511234d
Mark Schatza <markschatza@gmail.com>
2020-03-03 12:22:10 -0600

    Update with new Coh/Spec plugin

--- install_windows_2017.bat
+++ install_windows_2017.bat
@@ -67,7 +67,7 @@ call :download_repo "Crossing Detector"     tne-lab             crossing-detecto
 call :download_repo "Sample Math"           tne-lab             sample-math         cmake-gui   || exit /b 1
 call :download_repo "Mean Spike Rate"       tne-lab             mean-spike-rate     cmake-gui   || exit /b 1
 call :download_repo "ICA"                   tne-lab             ica-plugin          cmake-gui   || exit /b 1
-call :download_repo "Coherence"             tne-lab             coherence-viewer    master      || exit /b 1
+call :download_repo "Coherence"             tne-lab             Coherence-Spectrogram-Viewer    master      || exit /b 1
 if defined CONDA_HOME (
     call :download_repo "PythonPlugin"      tne-lab             PythonPlugin        cmake_build || exit /b 1          
 )
@@ -85,7 +85,7 @@ call :build_repo "Crossing Detector"        crossing-detector\CrossingDetector  
 call :build_repo "Sample Math"              sample-math\SampleMath              OE_PLUGIN_SampleMath        INSTALL     || exit /b 1
 call :build_repo "Mean Spike Rate"          mean-spike-rate\MeanSpikeRate       OE_PLUGIN_MeanSpikeRate     INSTALL     || exit /b 1
 call :build_repo "ICA"                      ica-plugin\ICA                      OE_PLUGIN_ICA               INSTALL     || exit /b 1
-call :build_repo "Coherence"                coherence-viewer\CoherenceViewer    OE_PLUGIN_CoherenceViewer   INSTALL     || exit /b 1
+call :build_repo "Coherence"                Coherence-Spectrogram-Viewer\CoherenceSpectrogramViewer    OE_PLUGIN_CoherenceSpectrogramViewer   INSTALL     || exit /b 1
 
 rem make links to the executable
 PowerShell -ExecutionPolicy RemoteSigned "$s=(New-Object -COM WScript.Shell).CreateShortcut('%rootdir%\open-ephys (%config%).lnk');$s.TargetPath='%rootdir%\plugin-GUI\Build\%config%\open-ephys.exe';$s.Save()"
